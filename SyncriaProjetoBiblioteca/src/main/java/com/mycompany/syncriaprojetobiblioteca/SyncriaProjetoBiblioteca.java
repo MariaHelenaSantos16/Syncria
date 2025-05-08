1016d4d22d00965af854de4297c1b1de68d3559c
ALUNO <ALUNO@DESKTOP-PD34CM9>
2025-05-08 15:53:19 -0300

    Estrutura de projeto do java

---
+++ SyncriaProjetoBiblioteca/src/main/java/com/mycompany/syncriaprojetobiblioteca/SyncriaProjetoBiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.syncriaprojetobiblioteca;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class SyncriaProjetoBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
