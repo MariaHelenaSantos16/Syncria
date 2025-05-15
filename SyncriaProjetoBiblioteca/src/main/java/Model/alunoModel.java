ba3335ca3265da0d08b73e1bd6cab39622671107
ALUNO <ALUNO@DESKTOP-11226VK>
2025-05-15 16:36:51 -0300

    Criação da classe alunoModel(variáveis)

---
+++ SyncriaProjetoBiblioteca/src/main/java/Model/alunoModel.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class alunoModel {
+    
+ private String nomeAlluno;
+private String turmaAluno;
+private String bimestreAluno;
+ private int quantidadeDeLivrosEmDivida;
+ private int quantidadeDeLivrosEmprestados;
+ private int quantidadeDeLivrosDevolvidos;
+ private String livrosAlugadosPeloAluno;
+    
+    
+    
+  
+
+    public alunoModel(String nomeAlluno, String turmaAluno, String bimestreAluno, int quantidadeDeLivrosEmDivida, int quantidadeDeLivrosEmprestados, int quantidadeDeLivrosDevolvidos, String livrosAlugadosPeloAluno) {
+        this.nomeAlluno = nomeAlluno;
+        this.turmaAluno = turmaAluno;
+        this.bimestreAluno = bimestreAluno;
+        this.quantidadeDeLivrosEmDivida = quantidadeDeLivrosEmDivida;
+        this.quantidadeDeLivrosEmprestados = quantidadeDeLivrosEmprestados;
+        this.quantidadeDeLivrosDevolvidos = quantidadeDeLivrosDevolvidos;
+        this.livrosAlugadosPeloAluno = livrosAlugadosPeloAluno;
+    }
+    
+    
+      public alunoModel() {
+    }
+  
+    
+    //Getter e Setter
+
+    public String getNomeAlluno() {
+        return nomeAlluno;
+    }
+
+    public void setNomeAlluno(String nomeAlluno) {
+        this.nomeAlluno = nomeAlluno;
+    }
+
+    public String getTurmaAluno() {
+        return turmaAluno;
+    }
+
+    public void setTurmaAluno(String turmaAluno) {
+        this.turmaAluno = turmaAluno;
+    }
+
+    public String getBimestreAluno() {
+        return bimestreAluno;
+    }
+
+    public void setBimestreAluno(String bimestreAluno) {
+        this.bimestreAluno = bimestreAluno;
+    }
+
+    public int getQuantidadeDeLivrosEmDivida() {
+        return quantidadeDeLivrosEmDivida;
+    }
+
+    public void setQuantidadeDeLivrosEmDivida(int quantidadeDeLivrosEmDivida) {
+        this.quantidadeDeLivrosEmDivida = quantidadeDeLivrosEmDivida;
+    }
+
+    public int getQuantidadeDeLivrosEmprestados() {
+        return quantidadeDeLivrosEmprestados;
+    }
+
+    public void setQuantidadeDeLivrosEmprestados(int quantidadeDeLivrosEmprestados) {
+        this.quantidadeDeLivrosEmprestados = quantidadeDeLivrosEmprestados;
+    }
+
+    public int getQuantidadeDeLivrosDevolvidos() {
+        return quantidadeDeLivrosDevolvidos;
+    }
+
+    public void setQuantidadeDeLivrosDevolvidos(int quantidadeDeLivrosDevolvidos) {
+        this.quantidadeDeLivrosDevolvidos = quantidadeDeLivrosDevolvidos;
+    }
+
+    public String getLivrosAlugadosPeloAluno() {
+        return livrosAlugadosPeloAluno;
+    }
+
+    public void setLivrosAlugadosPeloAluno(String livrosAlugadosPeloAluno) {
+        this.livrosAlugadosPeloAluno = livrosAlugadosPeloAluno;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
